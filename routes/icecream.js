ee348bf2b4cfab5be298893b311e0a68e87932c6
sahithyasiri <sahithyasirimalla27@gmail.com>
2023-04-14 00:20:21 -0500

    new coomit

--- routes/icecream.js
+++ routes/icecream.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('icecream', { title: 'Search Results icecream' });
-});
+///* GET home page. */
+//router.get('/', function(req, res, next) {
+  //res.render('icecream', { title: 'Search Results icecream' });
+//});
+
+//ymodule.exports = router;
+
+
+
+const icecream_controlers= require('../controllers/icecream');
+
+/* GET costumes */
+
+router.get('/', icecream_controlers.icecream_view_all_Page );
 
 module.exports = router;
