5fcee80d9964d16a3a1274b912902ae02f07f815
sahithyasiri <sahithyasirimalla27@gmail.com>
2023-04-19 18:04:04 -0500

    new commit

--- controllers/icecream.js
+++ controllers/icecream.js
@@ -102,6 +102,7 @@ exports.icecream_create_post = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+
 // Handle a show one view with id specified by query
 exports.icecream_view_one_Page = async function (req, res) {
     console.log("single view for id " + req.query.id)
@@ -109,6 +110,50 @@ exports.icecream_view_one_Page = async function (req, res) {
         result = await icecream.findById(req.query.id)
         res.render('icecreamdetail',
             { title: 'icecream Detail', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.icecream_create_Page = function (req, res) {
+    console.log("create view")
+    try {
+        res.render('icecreamcreate', { title: 'icecream Create' });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle building the view for updating a costume.
+// query provides the id
+exports.icecream_update_Page = async function (req, res) {
+    console.log("update view for item " + req.query.id)
+    try {
+        let result = await icecream.findById(req.query.id)
+        res.render('icecreamupdate', { title: 'icecream Update', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle a delete one view with id from query
+exports.icecream_delete_Page = async function (req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try {
+        result = await icecream.findById(req.query.id)
+        res.render('icecreamdelete', {
+            title: 'icecream Delete', toShow:
+                result
+        });
     }
     catch (err) {
         res.status(500)
--- routes/icecream.js
+++ routes/icecream.js
@@ -3,19 +3,28 @@ var router = express.Router();
 
 ///* GET home page. */
 //router.get('/', function(req, res, next) {
-  //res.render('icecream', { title: 'Search Results icecream' });
+//res.render('icecream', { title: 'Search Results icecream' });
 //});
 
 //ymodule.exports = router;
 
 
 
-const icecream_controlers= require('../controllers/icecream');
+const icecream_controlers = require('../controllers/icecream');
 
 /* GET costumes */
 
-router.get('/', icecream_controlers.icecream_view_all_Page );
+router.get('/', icecream_controlers.icecream_view_all_Page);
 
 module.exports = router;
+
 /* GET detail costume page */
 router.get('/detail', icecream_controlers.icecream_view_one_Page);
+/* GET create costume page */
+router.get('/create', icecream_controlers.icecream_create_Page);
+
+/* GET create update page */
+router.get('/update', icecream_controlers.icecream_update_Page);
+/* GET delete costume page */
+router.get('/delete', icecream_controlers.icecream_delete_Page);
+
